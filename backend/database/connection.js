d26a66636792336764c94356fc485e11a6614567
Utajara <j.chavez@blackstone.studio>
2023-01-30 16:02:12 -0600

    added connection environment variables

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,11 +1,11 @@
+require("dotenv").config()
 const knex = require('knex')({
   client: 'mysql',
   connection: {
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'db',
-    port: 30000
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DB,
   },
   pool: { min: 2, max: 7 }
 });
